cbe08d0600482247d86b4478f2db8298bda545a1
M1-Masa <masa455.wv@gmail.com>
2022-10-16 17:12:15 +0900

    add:test04

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Test03 to <a href="https://nextjs.org">Next.js!</a>
+          Test04 to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
